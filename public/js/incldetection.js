ee3d00917dd90de4ab78a7c691c5951c75013817
nakahata128 <41355062+nakahata128@users.noreply.github.com>
2018-08-08 11:15:04 +0900

    Update incldetection.js

--- public/js/incldetection.js
+++ public/js/incldetection.js
@@ -1,6 +1,6 @@
 // 座標から傾きを計算 引数はctrack.getCurrentPosition()の返り値
 function BiasScore(position) {
-	// 返り値 (理論値)0～1.0 （実際の値）0～0.5
+	// 返り値 0～1.57
 	var Score;
 
 	// 座標を取得できているかを確認
@@ -11,14 +11,14 @@ function BiasScore(position) {
 		const RightXPoint = position[28][0];
 		const RightYPoint = position[28][1];
 
-		// 目の角度を計算
+		// 目の角度を計算 0～3.14
 		var EyeRadian  = Math.atan2(RightXPoint - LeftXPoint, RightYPoint - LeftYPoint);
 
 		// 角度の絶対値を計算
-		Score = Math.abs(EyeRadian);
+		Score = Math.abs(EyeRadian-Math.PI/2);
 	}else{
 		// 座標を取得できなかった場合は、計測できる最大の角度を設定
-		Score = 0.5;
+		Score = Math.PI/2 ;
 	}
 
 	return Score ;
