8f9cfff5c9d8a4c4e5fac178d860cf063b47b369
sycq <shunsuke.yamabe.cq@hitachi-systems.com>
2018-08-08 12:06:53 +0900

    消える処理できた

--- public/js/huectl.js
+++ public/js/huectl.js
@@ -6,19 +6,25 @@ const url = "https://192.168.100.114/api/xRQtVO0TCT5yzA5uawrUAFGfiOdOOv4-r7rR06Y
 
 function changeColor(prm) {
 
-    const a = 46920 / 0.7;
-    let y = 46920 - a * prm;
+    const a = 46920 / 0.5;
+    let hue = 46920 - a * prm;
 
-    y = y < 0 ? 0 : y;
+    hue = hue < 0 ? 0 : hue;
 
     // あまり早すぎると動かないので、700msに一度だけ色を更新する
     if (new Date().getTime() - lastPut.getTime() > 700) {
-        var data = {
+        let data = {
             "on": true,
             "bri": 255,
-            "hue": Math.round(y),
+            "hue": Math.round(hue),
             "sat": 255
         };
+        if(0.5 < prm){
+            const a2 = 255 / 0.3
+            let bri = 255 - (prm - 0.5) * a2
+            bri = bri < 0 ? 0 : bri;
+            data.bri = Math.round(bri);
+        }
         var json = JSON.stringify(data);
 
         var xhr = new XMLHttpRequest();
